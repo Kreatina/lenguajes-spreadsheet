fa60ba9f7ebfc60254a47a7d8070f1dc451077c0
agustin-saravia <agustinjap21@gmail.com>
2022-06-15 21:12:45 -0300

    Added endpoint search for Ids

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
-console.log(require("./sheets"))
-
-const { getSheet } = require("./sheets");
-const { getUrl } = require("./urlHandler");
-const { getWiki } = require("./wiki");
-
+const {
+    getSheet
+} = require("./sheets");
+const fetch = require('node-fetch')
+const {
+    getWikiData
+} = require('./wiki');
+const wdk = require('wikidata-sdk');
+const wbk = require('wikibase-sdk');
 
 
 (async function () {
-    const wiki = await getWiki();
-    const apiUrl = await getUrl();
-    console.log(apiUrl)
+
+
     const sheet = await getSheet();
+    const rows = await sheet.getRows();
+    for (row of rows) {
+        
+        if (row.wikidataId !== '') {
+            continue
+        }
+        const searchConfig = {
+            search: row.name,
+            format: 'json',
+            language: 'en',
+            limit: 1,
+            haswbstatement: 'P31=Q34770',
+        }
+        const url = await wdk.cirrusSearchPages(searchConfig)
+        console.log(url)
+        const data = await fetch(url, {
+            headers: {
+                'User-Agent': 'WikiSheetsBot/0.1'
+            }
+        })
+        .then(res => res.json())
+        
+        console.log(data.query.search)
+        
+
+    }
     // console.log(sheet)
 
 }());
