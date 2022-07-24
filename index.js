5d854181803d880a7ec86dde371ba242c9503a1e
agustin-saravia <agustinjap21@gmail.com>
2022-07-24 17:33:00 -0300

    Implemented search function and updated wikidataId row

--- index.js
+++ index.js
@@ -1,45 +1,49 @@
+const { getSheet, getCell } = require("./sheets");
+
+const fetch = require("node-fetch");
+
+const wdk = require("wikidata-sdk");
 const {
-    getSheet
-} = require("./sheets");
+  GoogleSpreadsheetRow,
+  GoogleSpreadsheet,
+  GoogleSpreadsheetCell,
+} = require("google-spreadsheet");
+const { default: wiki } = require("wikipedia/dist");
 
-const fetch = require('node-fetch')
+async function searchWikipediaByTitle(title) {
+  const searchConfig = {
+    search: title,
+    format: "json",
+    language: "en",
+    limit: 1,
+    haswbstatement: "P31=Q34770",
+  };
+  const url = await wdk.cirrusSearchPages(searchConfig);
+  const data = await fetch(url, {
+    headers: {
+      "User-Agent": "WikiSheetsBot/0.1",
+    },
+  }).then((res) => res.json());
 
-const wdk = require('wikidata-sdk');
-const { GoogleSpreadsheetRow, GoogleSpreadsheet } = require("google-spreadsheet");
-
+  return data.query.search;
+}
 
 (async function () {
-    const sheet = await getSheet();
-    const rows = await sheet.getRows();
-    for (row of rows) {
-        
-        if (row.wikidataId !== '') {
-            continue
-        }
-        const searchConfig = {
-            search: row.name,
-            format: 'json',
-            language: 'en',
-            limit: 1,
-            haswbstatement: 'P31=Q34770',
-        }
-        const url = await wdk.cirrusSearchPages(searchConfig)
-        const data = await fetch(url, {
-            headers: {
-                'User-Agent': 'WikiSheetsBot/0.1'
-            }
-        })
-        .then(res => res.json())
-        
-        
-        const fetchedData = data.query.search
-        // console.log(fetchedData)
-        fetchedData.filter(prop =>{
-            const titles = prop.title
-            console.log(titles)
-         
-        })
-       
+  const sheet = await getSheet();
+  const rows = await sheet.getRows();
+  for (row of rows) {
+    if (row.wikidataId !== "") {
+      continue;
     }
 
-}());
+    const fetchedData = await searchWikipediaByTitle(row.name);
+    if (fetchedData.length == 0) continue;
+
+    const wikidataId = fetchedData[0].title;
+    row.wikidataId = wikidataId;
+    await row.save();
+
+    console.log(wikidataId);
+    console.log(rows.indexOf(row) + "/" + rows.length);
+  }
+})();
