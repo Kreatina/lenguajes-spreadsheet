e3d160f70727b15bbb67f89b186f255f768aa789
agustin-saravia <agustinjap21@gmail.com>
2022-07-24 17:35:15 -0300

    Minor fixes

--- sheets.js
+++ sheets.js
@@ -1,10 +1,11 @@
-
-const {GoogleSpreadsheet} = require('google-spreadsheet')
+const {
+    GoogleSpreadsheet
+} = require('google-spreadsheet')
 const creds = require('./client_secret.json');
 
- const getSheet = async ()=>{
-    
-    const doc = new GoogleSpreadsheet('1iEAntS4gM308YhkGsPS0X_znCwjWC4q2Cl6GVDYo2hA');
+const getSheet = async () => {
+
+    const doc = new GoogleSpreadsheet('1PqSVi0uqcw7kLK3_9RECCcWYg4zr_sXntwcrZc4m72c'); 
 
     await doc.useServiceAccountAuth(creds);
 
@@ -12,6 +13,26 @@ const creds = require('./client_secret.json');
 
     const sheet = doc.sheetsByIndex[0];
     return sheet
-
 }
-module.exports = {getSheet}
+
+
+
+
+
+module.exports = {
+    getSheet
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ wiki.js
@@ -0,0 +1,18 @@
+const wdk = require('wikidata-sdk')
+const {getSheet} =  require('./sheets')
+
+
+const getFromWiki = async () =>{
+   
+    
+}
+const getUrlTitle = async () =>{
+   getSheet.call(sheet.title = title)
+   console.log(title)
+
+  const url = `https://www.wikidata.org/w/api.php?action=wbgetentities&title=${title}&format=json`
+  
+
+}
+
+module.exports = {getFromWiki, getUrlTitle}
